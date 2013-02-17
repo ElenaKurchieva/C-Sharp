9e6f4a3364cc0f5973e13eadae91e5f54ae16c56
Anubis-Black <anubisblackbg@yahoo.com>
2013-02-17 23:35:22 +0200

    initial commit

---
+++ NextDate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class NextDate
+{
+    static void Main()
+    {
+        byte day = byte.Parse(Console.ReadLine());
+        byte month = byte.Parse(Console.ReadLine());
+        ushort year = ushort.Parse(Console.ReadLine());
+
+        if ((day == 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)) ||
+            (day == 30 && (month == 4 || month == 6 || month == 9 || month == 11)) ||
+            (day == 28 && month == 2 && !(year == 2000 || year == 2004 || year == 2008 || year == 2012)) ||
+            (day == 29 && month == 2 && (year == 2000 || year == 2004 || year == 2008 || year == 2012)))
+        {
+            day = 1;
+            if (month == 12)
+            {
+                month = 1;
+                year++;
+            }
+            else
+            {
+                month++;
+            }
+        }
+        else
+        {
+            day++;
+        }
+        Console.WriteLine(day + "." + month + "." + year);
+    }
+}
