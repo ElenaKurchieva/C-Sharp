ff371ff97c8183a833a1cdebc10ba9b637a20335
Anubis-Black <anubisblackbg@yahoo.com>
2013-04-06 19:52:09 +0300

    Latest homework from High Quality Code

---
+++ HighQualityCode/CodeFormatting/MenuFuctions/MenuFunctions.js
@@ -0,0 +1,121 @@
+﻿;(function () {
+    "use strict";
+
+    var applicationName = navigator.appName;
+    var addScroll = false;
+
+    if ((navigator.userAgent.indexOf("MSIE 5") > 0) || (navigator.userAgent.indexOf("MSIE 6")) > 0) {
+        addScroll = true;
+    }
+
+    var pX = 0;
+    var pY = 0;
+    var theLayer;
+
+    document.onmousemove = mouseMove;
+    if (applicationName === "Netscape") {
+        document.captureEvents(event.MOUSEMOVE);
+    }
+
+    function mouseMove(evn) {
+        if (applicationName === "Netscape") {
+            pX = evn.pageX - 5;
+            pY = evn.pageY;
+        }
+        else {
+            pX = event.x - 5;
+            pY = event.y;
+        }
+
+        if (applicationName === "Netscape") {
+            if (document.layers.ToolTip.visibility === "show") {
+                popTip();
+            }
+        }
+        else if (document.all.ToolTip.style.visibility === "visible") {
+            popTip();
+        }
+    }
+
+    function popTip() {
+        if (applicationName === "Netscape") {
+            theLayer = document.layers.ToolTip;
+            if ((pX + 120) > window.innerWidth) {
+                pX = window.innerWidth - 150;
+            }
+
+            theLayer.left = pX + 10;
+            theLayer.top = pY + 15;
+            theLayer.visibility = "show";
+        }
+        else {
+            theLayer = document.all.ToolTip;
+            if (theLayer) {
+                pX = event.x - 5;
+                pY = event.y;
+
+                if (addScroll) {
+                    pX = pX + document.body.scrollLeft;
+                    pY = pY + document.body.scrollTop;
+                }
+
+                if ((pX + 120) > document.body.clientWidth) {
+                    pX = pX - 150;
+                }
+
+                theLayer.style.pixelLeft = pX + 10;
+                theLayer.style.pixelTop = pY + 15;
+                theLayer.style.visibility = "visible";
+            }
+        }
+    }
+
+    function HideTip() {
+        if (applicationName === "Netscape") {
+            document.layers.ToolTip.visibility = "hide";
+        }
+        else {
+            document.all.ToolTip.style.visibility = "hidden";
+        }
+    }
+
+    function HideMenu1() {
+        if (applicationName === "Netscape") {
+            document.layers.menu1.visibility = "hide";
+        }
+        else {
+            document.all.menu1.style.visibility = "hidden";
+        }
+    }
+
+    function ShowMenu1() {
+        if (applicationName === "Netscape") {
+            theLayer = document.layers.menu1;
+            theLayer.visibility = "show";
+        }
+        else {
+            theLayer = document.all.menu1;
+            theLayer.style.visibility = "visible";
+        }
+    }
+
+    function HideMenu2() {
+        if (applicationName === "Netscape") {
+            document.layers.menu2.visibility = "hide";
+        }
+        else {
+            document.all.menu2.style.visibility = "hidden";
+        }
+    }
+
+    function ShowMenu2() {
+        if (applicationName === "Netscape") {
+            theLayer = document.layers.menu2;
+            theLayer.visibility = "show";
+        }
+        else {
+            theLayer = document.all.menu2;
+            theLayer.style.visibility = "visible";
+        }
+    }
+})();
